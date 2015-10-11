d039a111c89c9a7f50489d4b578156dbdaa9ab1a
Ktano <Pedrocaetano56@gmail.com>
2015-10-11 14:27:27 +0100

    Função atribui mandato completa
    A função atribui mandato esta agora completa. Atribui o proximo mandato
    ao tuplo de mandatos atraves do metodo de D'hondt. inclui a modificação
    que em caso de empate o mandato e atribuido a candidatura com menor
    numero de votos totais

--- projecto1.py
+++ projecto1.py
@@ -1,14 +1,17 @@
 def mandatos (nr_mandatos,nr_votos):
     mandato=1
-    nr_candidaturas = len(nr_votos) 
-    mandatos_atribuidos = cria_mandatos(nr_candidaturas) #cria um tuplo com o comprimento igual ao numero de candidaturas
+    nr_candidaturas = len(nr_votos)
+    #e necessario um tuplo com o comprimento igual ao numero de candidaturas
+    mandatos_atribuidos = cria_mandatos(nr_candidaturas) 
     while mandato <= nr_mandatos:
         mandatos_atribuidos = atribui_mandato(nr_votos,mandatos_atribuidos)
         mandato+=1
+    return mandatos_atribuidos
 
 def cria_mandatos(candidaturas):
     """
-    Esta funcao cria um tuplo com o comprimento=candidaturas e 0 em todas as entradas
+    Esta funcao cria um tuplo com o comprimento=candidaturas e 0 em todas
+    as entradas
     """
     mandatos_zero=(0,)
     while len(mandatos_zero)<candidaturas:
@@ -17,14 +20,27 @@ def cria_mandatos(candidaturas):
 
 def atribui_mandato(votos,mandatos_atuais):
     """"
-    Retorna um tuplo de mandatos com mais um mandato atribuido à proxima
+    Retorna um tuplo de mandatos com mais um mandato atribuido a proxima
     candidatura de acordo com o metodo de D'Hondt
     """
-    candi_actual=0
     total_candi = len(mandatos_atuais)
+    #inicia as variaveis para percorrer todas as candidaturas
+    candi_actual=0
     candi_venc=candi_actual
-    votos = Votos(candidatura_vencedora)/(mandatos_atuais(candidatura_vencedora)+1)
+    #os votos para comparar sao calculados atraves do numero de votos a dividir
+    #pelo numero de deputados ja atribuidos a candidatura mais 1
+    votos_venc = votos[candi_venc]/(mandatos_atuais[candi_venc]+1)
     
-    while candi_atual<=(total_candi-1):
+    while candi_actual<(total_candi-1):
+        candi_actual+=1
+        votos_act = votos[candi_actual]/(mandatos_atuais[candi_actual]+1)
+        #se a candidatura actual tiver mais votos ajustados que a vencedora a
+        #actual passa a ser a vencedor se existir um empate a que tem menor
+        #numero de votos total ganha
+        if votos_venc < votos_act or \
+           (votos_venc==votos_act and votos[candi_actual]<votos[candi_venc]):
+            candi_venc=candi_actual
+            votos_venc = votos_act
         
-        if votos <
+    return mandatos_atuais[:candi_venc] + (mandatos_atuais[candi_venc]+1,) +\
+           mandatos_atuais[candi_venc+1:]
